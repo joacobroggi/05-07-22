9dbc9a3a1fbbcc5ffeec0baef964153831063b6a
joaco_broggi74 <playstationgta84@gmail.com>
2022-07-05 20:03:04 -0300

    v1.2.0

--- js/index.js
+++ js/index.js
@@ -1,8 +1,2 @@
-let card = document.querySelector('.cardd');
 
-card.addEventListener('click', function () {
-    window.location.replace("/canciones.html");
-})
-
-console.log(window.location.href);
 
